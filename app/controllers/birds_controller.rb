715ec2297bb59421939a6bc06a2604d379410885
github username <github email address>
2020-10-24 16:16:58 -0400

    Done.

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,6 +1,13 @@
+
+
 class BirdsController < ApplicationController
   def index
     @birds = Bird.all
-    render 'birds/index.html.erb'
+    render json: { birds: @birds, messages: ['Hello Birds', 'Goodbye Birds'] }
+    # render json: @birds 
+    #In our bird watching case, we actually already have a collection of data, @birds
+    # render plain: "Hello #{@birds[3].name}" 
+#To render plain text from a Rails controller, you specify plain:, followed by the text you want to display:
+    # render 'birds/index.html.erb' = just displays ERB
   end
 end
